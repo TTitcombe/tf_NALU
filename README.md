3cb39dbd9183d8ca4137329d19b6ccc60a7d6e1a
Tom Titcombe <t.j.titcombe@gmail.com>
2018-09-24 16:10:36 +0100

    Results for small interpolation experiment

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 # TF Neural Arithmetic Logic Unit
 
-A tensorflow implementation of the paper ["Neural Arithmetic Logic Unit"](https://arxiv.org/pdf/1808.00508.pdf) by Andrew Trask, Felix Hill, Scott Reed, Jack Rae, Chris Dyer, Phil Blunsom.
+A tensorflow implementation of the paper ["Neural Arithmetic Logic Units"](https://arxiv.org/pdf/1808.00508.pdf) by Andrew Trask, Felix Hill, Scott Reed, Jack Rae, Chris Dyer, Phil Blunsom.
 
 Explanation of the paper, and a write-up of this code, can be found [here](https://medium.com/@t.j.titcombe/understanding-neural-arithmetic-logic-units-5ca9d0041473).
 
-This code currently provides a NAC module, a NALU module, and a deep network of stacked NALU modules.
-The experiments in section 4.1 of the paper have been implemented.
-To test, run **test_nalu.py**
+This code currently provides a NAC module, a NALU module, and a MLP.
 
-### TODO
+The experiments in section 4.1 of the paper have been implemented. (Static numerical interpolation and extrpolation)
+
+To test, run **test_nalu.py**.
+
+## Results
+Currently have only performed the experiments of section 4.1 of the paper, the static numerical tests (addition, subtraction, multiplication, division, square, square root).
+
+In the results directory are several text files, for various experiments.
+
+Naming convention:
+* I for interpolation, E for extrapolation
+* add/subtract/multiply etc. defines the operation used to calculate the target data
+* "small". In small experiments, the x data has 2 dimensions. These dimensions were e.g. multiplied together in the multiplication test.
+If small is not present in the name, then x data has 100 dimensions. Two samples of size 10 (without replacement) were taken. The data in each sample are summed together. These sums are then e.g. multiplied together for the multiplication experiment.
+
+## TODO
 * Hyperparameter tune
 * Perform numerical extrapolation experiment (section 1.1. in original paper)
     - Graph of mean absolute error against input value does not match the paper well
 * Test interpolation of static numerical tests
-    - NALU and NAC both outperformed by relu6 on interpolation addition. Is this due to poor tuning or a bug?
+    - Small data: NAC outperforms relu6 on addition and subtraction, but NALU does not. Bug or tuning?
+    - Small data: NALU outperforms relu6 and NAC on multiplication, division etc.
+    - NALU and NAC both outperformed by relu6 on large interpolation addition.
 * Test extrapolation of static numerical tests
     - Succeed in interpolation first
-
--- test_nalu.py
+++ test_nalu.py
@@ -10,10 +10,10 @@ operations = {
 'multiply': lambda x,y: x * y,
 'divide': lambda x, y: x / y,
 'square': lambda x, y: x**2,
-'root': lambda x, y: np.sqrt(x)
+'root': lambda x, y: np.sqrt(x + 1e-6)
 }
 
-def generate_data(n,z=100, min=-1., max=1., select = 10, op = 'add', seed=42):
+def generate_data(n,z=2, min=-1., max=1., select = 1, op = 'add', seed=42):
     global operations
 
     if seed is not None:
@@ -30,7 +30,7 @@ def generate_data(n,z=100, min=-1., max=1., select = 10, op = 'add', seed=42):
 
     return x, y
 
-def test(models, x_train, y_train, x_Is, y_Is,
+def test(models, x_train, y_train, x_tests, y_tests,
             INPUT, OUTPUT, HIDDEN, batch_size,
             N_epochs, filename):
     global results_dir
@@ -48,41 +48,41 @@ def test(models, x_train, y_train, x_Is, y_Is,
         else:
             model = Model(INPUT, OUTPUT, HIDDEN, model_type=model_type)
             trainer = Trainer(model)
-            trainer.train(x, y, x_Is[0], y_Is[0], batch_size, N_epochs)
+            trainer.train(x, y, x_tests[0], y_tests[0], batch_size, N_epochs)
 
-        for x_I, y_I in zip(x_Is, y_Is):
-            err, loss = trainer._validate(x_I, y_I)
+        for x_test, y_test in zip(x_tests, y_tests):
+            err, loss = trainer._validate(x_test, y_test)
             losses[model_type] += loss
         losses[model_type] /= N_test_data
 
     with open(results_dir + filename, "w") as f:
         for k, v in losses.items():
-            f.write("{}: {} \n".format(k,round(v,2)))
+            f.write("{}: {} \n".format(k,round(v,5)))
 
 
 if __name__ == '__main__':
     results_dir = 'results/'
 
-    _op = 'add'
-    x, y = generate_data(1000, op=_op)
-    x_Is = []
-    y_Is = []
-
-    N_tests = 100
-    for i in range(N_tests):
-        x_I, y_I = generate_data(100, op=_op)
-        x_Is.append(x_I)
-        y_Is.append(y_I)
-
-    #x_E, y_E = generate_data(100, min=-10., max=10., op=_op)
-
-    INPUT = 100
+    INPUT = 2
     OUTPUT = 1
     HIDDEN = [2]
+    MIN = -10.
+    MAX = 10.
 
-    N_epochs = int(1e2)
+    N_epochs = int(1e4)
     batch_size = 100
+    for _op in ['add', 'subtract', 'multiply', 'divide', 'square', 'root']:
+        #_op = 'subtract'
+        x, y = generate_data(1000, z=INPUT, op=_op)
+        x_tests = []
+        y_tests = []
 
-    models = ['nac', 'nalu', 'relu6', 'random']
-    test(models, x, y, x_Is, y_Is, INPUT, OUTPUT, HIDDEN,
-            batch_size, N_epochs, "interpolation.txt")
+        N_tests = 100
+        for i in range(N_tests):
+            x_test, y_test = generate_data(100,min=MIN, max=MAX, op=_op)
+            x_tests.append(x_test)
+            y_tests.append(y_test)
+
+        models = ['nac', 'nalu', 'relu6', 'random']
+        test(models, x, y, x_tests, y_tests, INPUT, OUTPUT, HIDDEN,
+                batch_size, N_epochs, "E_{}_small.txt".format(_op))
